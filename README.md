ee5314925b5ba6a036c76a29ba711d26da449137
Cody Craven <cody@cravencode.com>
2020-02-06 15:57:33 -0800

    Begin dep installation script

---
+++ README.md
@@ -0,0 +1,12 @@
+# CodyCraven's Dotfiles
+
+Version control and tooling for dot files for consistent environment.
+
+## Usage
+
+Check/install environment dependencies:
+
+```sh
+./deps.sh
+```
+
---
+++ deps.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+RAN_UPDATE=0
+
+update() {
+	if [ $RAN_UPDATE -eq 0 ]; then
+		tput smso
+		echo "Updating package information"
+		tput rmso
+		sudo apt update
+	fi
+	RAN_UPDATE=1
+}
+
+check_app() {
+	if ! command -v $1 >/dev/null; then
+		update
+		tput smso
+		echo "Installing missing $2"
+		tput rmso
+		sudo apt install "$2" -y
+	fi
+}
+
+check_app nvim neovim
+
