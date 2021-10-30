dadcb06cf83bb3b8dca616099faadb92ca234d4d
Cody Craven <cody@cravencode.com>
2021-10-29 23:27:32 -0700

    Fix .npmrc file mount that stopped working in recent Docker

--- copy.sh
+++ copy.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 copy_files_from_dir_to_dir() {
-	local FROM="$1"
+	local FROM=$(echo "$1" | sed 's/\//\\\//g')
 	local TO=$(echo "$2" | sed 's/\//\\\//g')
+	local CP_FLAG="$3"
+	if [ -n "$CP_FLAG" ]; then
+		CP_FLAG="$CP_FLAG "
+	fi
 	tput smso
-	echo "Copying files from \`./$FROM\` to \`$2\`"
+	echo "Copying files from \`./$1\` to \`$2\`"
 	tput rmso
-	find "./$FROM/" -type f | sed -n '
+	find "./$1/" -type f | sed -n '
 	# save frompath
 	h
 	# convert to topath
@@ -23,12 +27,12 @@ copy_files_from_dir_to_dir() {
 	# transform frompath\ntopath, to print:
 	# - mkdir -p todir
 	# - cp frompath topath
-	s/^\(.*\)\n\(.*\)\(\/.*\)$/mkdir -p "\2"; cp "\1" "\2\3"/p
+	s/^\(.*\)\n\(.*\)\(\/.*\)$/mkdir -p "\2"; cp '"${CP_FLAG}"'"\1" "\2\3"/p
 	' | bash
 }
 
 copy_files_from_dir_to_dir home "$HOME"
-
+copy_files_from_dir_to_dir conditional/home "$HOME" --no-clobber
 
 tput smso
 echo "Installing neovim plugins"
